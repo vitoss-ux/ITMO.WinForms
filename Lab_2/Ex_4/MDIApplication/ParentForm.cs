2d69d4f95d3569df863034408e1c5a4c2a6ee363
Vitaliy <73466603+vitoss-ux@users.noreply.github.com>
2021-01-26 19:06:28 +0300

    Add files via upload

---
+++ Lab_2/Ex_4/MDIApplication/ParentForm.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MDIApplication
+{
+    public partial class ParentForm : Form
+    {
+        private int openDocuments = 0;
+
+        public ParentForm()
+        {
+            InitializeComponent();
+            // Свойству Text панели spData устанавливается текущая дата 
+            spData.Text = Convert.ToString(System.DateTime.Today.ToLongDateString());
+        }
+
+        private void ExitMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void WindowCascadeMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.Cascade);
+            spWin.Text = "Windows is cascade";
+        }
+
+        private void WindowTileMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.TileHorizontal);
+            spWin.Text = "Windows is horizontal";
+        }
+
+        private void NewMenuItem_Click(object sender, EventArgs e)
+        {
+            ChildForm newChild = new ChildForm();
+            newChild.Text = newChild.Text + " " + ++openDocuments;
+            newChild.MdiParent = this;
+            newChild.Show();
+        }
+
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            switch (e.ClickedItem.Tag.ToString())
+            {
+                case "New Doc":
+                    ChildForm newChild = new ChildForm();
+                    newChild.MdiParent = this;
+                    newChild.Show();
+                    newChild.Text = newChild.Text + " " + ++openDocuments;
+                    break;
+                case "Cascade":
+                    LayoutMdi(System.Windows.Forms.MdiLayout.Cascade);
+                    spWin.Text = "Windows is cascade";
+                    break;
+                case "Title":
+                    LayoutMdi(System.Windows.Forms.MdiLayout.TileHorizontal);
+                    spWin.Text = "Windows is horizontal";
+                    break;
+            }
+        }
+
+        private void ParentForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

ae322464f0720d485c64ef9cbde789fe58bf5ad9
Vitaliy <73466603+vitoss-ux@users.noreply.github.com>
2021-01-26 18:52:41 +0300

    Add files via upload

---
+++ Lab_1/Ex_3/WindowsFormsApp2/nForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class nForm : WindowsFormsApp2.Form1
+    {
+        public nForm()
+        {
+            InitializeComponent();
+        }
+    }
+}

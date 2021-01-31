0a44ec89b169868405a186de57bc303900e10a5e
Vitaliy <73466603+vitoss-ux@users.noreply.github.com>
2021-01-31 13:47:38 +0300

    Add files via upload

---
+++ Lab_4/Ex_4/WindowsFormsOwner/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsOwner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string SolData
+        {
+            get
+            { return textBoxF1.Text; }
+
+            set
+            { textBoxF1.Text = value; }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 frm2 = new Form2();
+            frm2.Show(this);
+        }
+
+
+    }
+}
---
+++ Lab_4/Ex_4/WindowsFormsOwner/Form2.Designer.cs
@@ -0,0 +1,92 @@
+﻿
+namespace WindowsFormsOwner
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxF21 = new System.Windows.Forms.TextBox();
+            this.textBoxF22 = new System.Windows.Forms.TextBox();
+            this.textBoxF23 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxF21
+            // 
+            this.textBoxF21.Location = new System.Drawing.Point(126, 80);
+            this.textBoxF21.Name = "textBoxF21";
+            this.textBoxF21.Size = new System.Drawing.Size(279, 31);
+            this.textBoxF21.TabIndex = 0;
+            // 
+            // textBoxF22
+            // 
+            this.textBoxF22.Location = new System.Drawing.Point(126, 171);
+            this.textBoxF22.Name = "textBoxF22";
+            this.textBoxF22.Size = new System.Drawing.Size(279, 31);
+            this.textBoxF22.TabIndex = 1;
+            // 
+            // textBoxF23
+            // 
+            this.textBoxF23.Location = new System.Drawing.Point(126, 254);
+            this.textBoxF23.Name = "textBoxF23";
+            this.textBoxF23.Size = new System.Drawing.Size(279, 31);
+            this.textBoxF23.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(126, 354);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(241, 49);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxF23);
+            this.Controls.Add(this.textBoxF22);
+            this.Controls.Add(this.textBoxF21);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxF21;
+        private System.Windows.Forms.TextBox textBoxF22;
+        private System.Windows.Forms.TextBox textBoxF23;
+        private System.Windows.Forms.Button button1;
+    }
+}

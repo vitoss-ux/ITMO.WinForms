ecf723aeb80235e59981701ed4dbe2df456a31ab
Vitaliy <73466603+vitoss-ux@users.noreply.github.com>
2021-02-05 20:19:45 +0300

    Add files via upload

---
+++ Lab_3/Ex_1/WinTimer1/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace WinTimer1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.userControlTimer1 = new WinTimer1.UserControlTimer();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.Location = new System.Drawing.Point(125, 205);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 57);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "STOP!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // userControlTimer1
+            // 
+            this.userControlTimer1.Location = new System.Drawing.Point(71, 53);
+            this.userControlTimer1.Name = "userControlTimer1";
+            this.userControlTimer1.Size = new System.Drawing.Size(291, 146);
+            this.userControlTimer1.TabIndex = 2;
+            this.userControlTimer1.TimeEnabled = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(374, 329);
+            this.Controls.Add(this.userControlTimer1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Timer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private UserControlTimer userControlTimer1;
+    }
+}
+
---
+++ Lab_3/Ex_3/WinButNum/ClickButton.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinButNum
+{
+    public class ClickButton : Button
+    {
+        int mClicks;
+        public int Clicks
+        {
+            get { return mClicks; }
+        }
+
+        protected override void OnClick(EventArgs e)
+        {
+            mClicks++;
+            base.OnClick(e);
+        }
+
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            base.OnPaint(pevent);
+            Graphics g = pevent.Graphics;
+            SizeF stringsize;
+            stringsize = g.MeasureString(Clicks.ToString(), this.Font,
+            this.Width);
+            g.DrawString(Clicks.ToString(), this.Font,
+            SystemBrushes.ControlText,
+            this.Width - stringsize.Width - 3, this.Height - stringsize.Height - 3);
+        }
+    }
+}
